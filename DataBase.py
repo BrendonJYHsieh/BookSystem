5b394c0ba3ab6ae2e75c84a0b74808c6c4c51355
JunYao <B10815044@gapps.ntust.edu.tw>
2021-10-24 16:00:22 +0800

    DB Delete

--- DataBase.py
+++ DataBase.py
@@ -42,11 +42,46 @@ class DataBaseManager:
     self.cursor.execute(sql, val)
     self.mydb.commit()
 
+  def delete_room(self,RoomName):
+    sql = "DELETE FROM rooms WHERE RoomName = %s"
+    val = (RoomName)
+    params = (val,) # Due to single value
+    self.cursor.execute(sql, params)
+    self.mydb.commit()
+
+  def delete_event(self, EventID):
+    sql = "DELETE FROM events WHERE EventID = %s"
+    val = (EventID)
+    params = (val,) # Due to single value
+    self.cursor.execute(sql, params)
+    self.mydb.commit()
+  
+  def delete_participant(self, EventID, Email):
+    sql = "DELETE FROM participants WHERE EventID = %s AND Email = %s"
+    val = (EventID,Email)
+    self.cursor.execute(sql, val)
+    self.mydb.commit()
+
 DBM = DataBaseManager()
+### Create ###
 # DBM.create_room("test")
 # DBM.show_rooms()
+
 # DBM.create_event(11,"軟體工程", "Teacher:柯拉飛", '2021-10-15 13:20:00', '2021-10-15 15:10:00', "TR")
 # DBM.show_events()
+
 # DBM.create_participant(0,"testtest@gmail.com")
 # DBM.show_participants()
+
+### Delete ###
+# DBM.delete_room("sadsa")
+# DBM.show_rooms()
+
+# DBM.delete_event(11)
+# DBM.show_events()
+
+# DBM.delete_participant(0,"testtest@gmail.com")
+# DBM.show_participants()
+
+
 
