819c802912634b70c6acf92541fda09a03f7612d
frakw <frakwu@gmail.com>
2021-12-13 02:19:29 +0800

    participants 初始化

--- core/Event.py
+++ core/Event.py
@@ -9,13 +9,14 @@ class Event:
     description = ""
     start_time =""
     end_time =""
-    participants = None
+    participants = []
     def __init__(self,_BookSystem,_name,_description,_start_time,_end_time): #TODO: time
         self.BookSystem = _BookSystem
         self.name = _name
         self.description = _description
         self.start_time = _start_time
         self.end_time = _end_time
+        self.participants = []
         return
     def update_participants(self,_participants):
         self.participants = _participants
