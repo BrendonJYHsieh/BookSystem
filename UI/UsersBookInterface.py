0816a8e2399b6d7383b7377173eb55999b22adb5
B10815058 <65119128+B10815058@users.noreply.github.com>
2022-01-03 02:08:50 +0800

    修正eventlist不能下拉

--- UI/UsersBookInterface.py
+++ UI/UsersBookInterface.py
@@ -40,7 +40,21 @@ class UsersBookInterface(BaseInterface.BaseInterface):
     def UpdateEventList(self):
         for btn in self.eventList:
             btn.destroy()
+        if self.eventListGroup:
+            self.eventListGroup.destroy()
         self.eventList = []
+        
+        self.eventListGroup = tk.Frame(self.mainCanvas,height=400,width=400,bd=0,background="black")
+        self.eventListCanvas = tk.Canvas(self.eventListGroup,height=400,width=350,borderwidth=0, highlightthickness=0, background="#575757")
+        self.eventLIstScrollbar = tk.Scrollbar(self.eventListGroup, orient=VERTICAL,command=self.eventListCanvas.yview)
+        self.eventListCanvas.configure(yscrollcommand=self.eventLIstScrollbar.set)
+        self.eventListCanvas.bind('<Configure>',lambda e:self.eventListCanvas.configure(scrollregion=self.eventListCanvas.bbox("all")))
+        self.eventListSecondFram = tk.Frame(self.eventListCanvas,height=400,width=350,bd=0,background="#575757")
+        self.eventListCanvas.create_window((0,0),window=self.eventListSecondFram,anchor="nw")
+
+        self.eventListGroup.place(x=80,y=100)
+        self.eventListCanvas.pack(side=LEFT,fill=BOTH)
+        self.eventLIstScrollbar.pack(side=RIGHT,fill=Y)
         index = 0
         #print('-----------------------')
         print(self.bookSystem.auth.CurrentUser)
@@ -49,7 +63,6 @@ class UsersBookInterface(BaseInterface.BaseInterface):
         for event in events:
             self.CreateEvent(index, event)
             index += 1
-        pass
     def CreateEvent(self,_index, _event):
         eventBoxCanvas = tk.Canvas(self.eventListSecondFram,background='red', relief=SUNKEN, highlightthickness=0,width=280,height=80)
         eventlabel = tk.Label(eventBoxCanvas, text = _event.name, font=('Helvetica', '13'),background='red')
@@ -68,3 +81,13 @@ class UsersBookInterface(BaseInterface.BaseInterface):
         #modifyBtn.place(anchor='nw',x=50,y=30)
         self.eventList.append(eventBoxCanvas)
         pass
+    def CreateTestEvent(self,_index):
+        eventBoxCanvas = tk.Canvas(self.eventListSecondFram,background='red', relief=SUNKEN, highlightthickness=0,width=280,height=80)
+        eventlabel = tk.Label(eventBoxCanvas, text ="Test", font=('Helvetica', '13'),background='red')
+        #deleteBtn = tk.Button(eventBoxCanvas, text="delete")
+        #modifyBtn = tk.Button(eventBoxCanvas, text="modify")
+        eventBoxCanvas.grid(row=_index,column=0,padx=10,pady = 5)
+        eventlabel.place(anchor='nw',x=0,y=0)
+        #deleteBtn.place(anchor='nw',x=0,y=30)
+        #modifyBtn.place(anchor='nw',x=50,y=30)
+        self.eventList.append(eventBoxCanvas)
